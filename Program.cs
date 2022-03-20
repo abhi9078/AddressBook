d9905f0737f947ae839596e7f5b0c804fc63addd
Abhilash <abhilashmeher1234@gmail.com>
2022-03-20 16:04:21 +0530

    Added Program file for all UC case

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace AddressBookManagement
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To AddressBook Program");
-
+            //UC_1 Program
             /*UC_Program uC_1 = new UC_Program();
             Person person = new Person();
             person.First_Name = "Abhilash";
@@ -21,50 +21,54 @@ namespace AddressBookManagement
             uC_1.addPerson(person);
             uC_1.Display();*/
 
-           /* UC_Program uC_2 = new UC_Program();
-            Person person = new Person();
-            Console.WriteLine("Enter First Name: ");
-            string fname = Console.ReadLine();
-            person.First_Name = fname;
-            Console.WriteLine("Enter Last Name: ");
-            string lname = Console.ReadLine();
-            person.Last_Name = lname;
-            Console.WriteLine("Enter Address ");
-            string address = Console.ReadLine();
-            person.Address = address;
-            Console.WriteLine("Enter City: ");
-            string city = Console.ReadLine();
-            person.City = city;
-            Console.WriteLine("Enter State: ");
-            string state = Console.ReadLine();
-            person.State = state;
-            Console.WriteLine("Enter pin code: ");
-            int pin = int.Parse(Console.ReadLine());
-            person.PostalCode = pin;
-            Console.WriteLine("Enter Phone Number: ");
-            long number = long.Parse(Console.ReadLine());
-            person.PhoneNumber = number;
-            Console.WriteLine("Enter Email id: ");
-            string email = Console.ReadLine();
-            person.Email = email;
-            uC_2.addPerson(person);
-            uC_2.Display();*/
+            //UC_2 program
+            /* UC_Program uC_2 = new UC_Program();
+             Person person = new Person();
+             Console.WriteLine("Enter First Name: ");
+             string fname = Console.ReadLine();
+             person.First_Name = fname;
+             Console.WriteLine("Enter Last Name: ");
+             string lname = Console.ReadLine();
+             person.Last_Name = lname;
+             Console.WriteLine("Enter Address ");
+             string address = Console.ReadLine();
+             person.Address = address;
+             Console.WriteLine("Enter City: ");
+             string city = Console.ReadLine();
+             person.City = city;
+             Console.WriteLine("Enter State: ");
+             string state = Console.ReadLine();
+             person.State = state;
+             Console.WriteLine("Enter pin code: ");
+             int pin = int.Parse(Console.ReadLine());
+             person.PostalCode = pin;
+             Console.WriteLine("Enter Phone Number: ");
+             long number = long.Parse(Console.ReadLine());
+             person.PhoneNumber = number;
+             Console.WriteLine("Enter Email id: ");
+             string email = Console.ReadLine();
+             person.Email = email;
+             uC_2.addPerson(person);
+             uC_2.Display();*/
 
-           /* UC_Program uc_3 = new UC_Program();
-            uc_3.Display();
-            Console.WriteLine("Enter First name to Edit that particular Item: ");
-            string name=Console.ReadLine();
-            uc_3.EditPerson(name);
-            uc_3.Display();*/
+            //UC_3 program
+            /* UC_Program uc_3 = new UC_Program();
+             uc_3.Display();
+             Console.WriteLine("Enter First name to Edit that particular Item: ");
+             string name=Console.ReadLine();
+             uc_3.EditPerson(name);
+             uc_3.Display();*/
 
-           /* UC_Program uc_4 = new UC_Program();
-            uc_4.Display();
-            Console.WriteLine("Enter First name to Delete that particular Item: ");
-            string name = Console.ReadLine();
-            uc_4.Remove(name);
-            uc_4.Display();*/
+            //UC_4 program
+            /* UC_Program uc_4 = new UC_Program();
+             uc_4.Display();
+             Console.WriteLine("Enter First name to Delete that particular Item: ");
+             string name = Console.ReadLine();
+             uc_4.Remove(name);
+             uc_4.Display();*/
 
-            UC_Program uc_5 = new UC_Program();
+            //UC_5 program
+            /*UC_Program uc_5 = new UC_Program();
             
             while (true)
             {
@@ -107,8 +111,207 @@ namespace AddressBookManagement
                         uc_5.Display();
                         break;
                 }
-            }
+            }*/
 
+            //UC_6 program
+            Console.WriteLine("Please Select Any one AddressBook");
+            Console.WriteLine("1. Address Book 1");
+            Console.WriteLine("2. Address Book 2");
+            Console.WriteLine("3. Address Book 3");
+            int num = int.Parse(Console.ReadLine());
+            switch (num)
+            {
+                case 1:
+                    
+                        AddressBook1 ad1 = new AddressBook1();
+                        Console.WriteLine("Welcome to Address BOOK 1");
+                        while (true)
+                        {
+                            Console.WriteLine("\nSelect Any one Operation: ");
+                            Console.WriteLine("1. To Add New Person: ");
+                            Console.WriteLine("2. To Display Contacts: ");
+                            Console.WriteLine("3. To Edit any Contacts: ");
+                            Console.WriteLine("4. To Delete any Contacts: ");
+                            int read = int.Parse(Console.ReadLine());
+                            switch (read)
+                            {
+                                case 1:
+                                    Person person = new Person();
+                                    Console.WriteLine("Enter First Name: ");
+                                    string fname = Console.ReadLine();
+                                    person.First_Name = fname;
+                                    Console.WriteLine("Enter Last Name: ");
+                                    string lname = Console.ReadLine();
+                                    person.Last_Name = lname;
+                                    Console.WriteLine("Enter Address ");
+                                    string address = Console.ReadLine();
+                                    person.Address = address;
+                                    Console.WriteLine("Enter City: ");
+                                    string city = Console.ReadLine();
+                                    person.City = city;
+                                    Console.WriteLine("Enter State: ");
+                                    string state = Console.ReadLine();
+                                    person.State = state;
+                                    Console.WriteLine("Enter pin code: ");
+                                    int pin = int.Parse(Console.ReadLine());
+                                    person.PostalCode = pin;
+                                    Console.WriteLine("Enter Phone Number: ");
+                                    long number = long.Parse(Console.ReadLine());
+                                    person.PhoneNumber = number;
+                                    Console.WriteLine("Enter Email id: ");
+                                    string email = Console.ReadLine();
+                                    person.Email = email;
+                                    ad1.addPerson(person);
+                                    break;
+
+                                case 2:
+                                    ad1.Display();
+                                    break;
+                                case 3:
+                                    Console.WriteLine("Enter First name to Edit that particular Item: ");
+                                    string name = Console.ReadLine();
+                                    ad1.EditPerson(name);
+                                    break;
+                                case 4:
+                                    Console.WriteLine("Enter First name to Delete that particular Item: ");
+                                    string name1 = Console.ReadLine();
+                                    ad1.Remove(name1);
+                                    break;
+                                default:
+                                    Console.WriteLine("Please Select the proper operation");
+                                    break;
+                            }
+                        }
+                        break;
+                    
+                case 2:
+                    AddressBook2 ad2 = new AddressBook2();
+                    Console.WriteLine("Welcome to Address BOOK 2");
+                    while (true)
+                    {
+                        Console.WriteLine("\nSelect Any one Operation: ");
+                        Console.WriteLine("1. To Add New Person: ");
+                        Console.WriteLine("2. To Display Contacts: ");
+                        Console.WriteLine("3. To Edit any Contacts: ");
+                        Console.WriteLine("4. To Delete any Contacts: ");
+                        int read = int.Parse(Console.ReadLine());
+                        switch (read)
+                        {
+                            case 1:
+                                Person2 person2 = new Person2();
+                                Console.WriteLine("Enter First Name: ");
+                                string fname = Console.ReadLine();
+                                person2.First_Name = fname;
+                                Console.WriteLine("Enter Last Name: ");
+                                string lname = Console.ReadLine();
+                                person2.Last_Name = lname;
+                                Console.WriteLine("Enter Address ");
+                                string address = Console.ReadLine();
+                                person2.Address = address;
+                                Console.WriteLine("Enter City: ");
+                                string city = Console.ReadLine();
+                                person2.City = city;
+                                Console.WriteLine("Enter State: ");
+                                string state = Console.ReadLine();
+                                person2.State = state;
+                                Console.WriteLine("Enter pin code: ");
+                                int pin = int.Parse(Console.ReadLine());
+                                person2.PostalCode = pin;
+                                Console.WriteLine("Enter Phone Number: ");
+                                long number = long.Parse(Console.ReadLine());
+                                person2.PhoneNumber = number;
+                                Console.WriteLine("Enter Email id: ");
+                                string email = Console.ReadLine();
+                                person2.Email = email;
+                                ad2.addPerson(person2);
+                                break;
+
+                            case 2:
+                                ad2.Display();
+                                break;
+                            case 3:
+                                Console.WriteLine("Enter First name to Edit that particular Item: ");
+                                string name = Console.ReadLine();
+                                ad2.EditPerson(name);
+                                break;
+                            case 4:
+                                Console.WriteLine("Enter First name to Delete that particular Item: ");
+                                string name1 = Console.ReadLine();
+                                ad2.Remove(name1);
+                                break;
+                            default:
+                                Console.WriteLine("Please Select the proper operation");
+                                break;
+                        }
+                    }
+                    break;
+
+                case 3:
+                    AddressBook3 ad3 = new AddressBook3();
+                    Console.WriteLine("Welcome to Address BOOK 3");
+                    while (true)
+                    {
+                        Console.WriteLine("\nSelect Any one Operation: ");
+                        Console.WriteLine("1. To Add New Person: ");
+                        Console.WriteLine("2. To Display Contacts: ");
+                        Console.WriteLine("3. To Edit any Contacts: ");
+                        Console.WriteLine("4. To Delete any Contacts: ");
+                        int read = int.Parse(Console.ReadLine());
+                        switch (read)
+                        {
+                            case 1:
+                                Person3 person = new Person3();
+                                Console.WriteLine("Enter First Name: ");
+                                string fname = Console.ReadLine();
+                                person.First_Name = fname;
+                                Console.WriteLine("Enter Last Name: ");
+                                string lname = Console.ReadLine();
+                                person.Last_Name = lname;
+                                Console.WriteLine("Enter Address ");
+                                string address = Console.ReadLine();
+                                person.Address = address;
+                                Console.WriteLine("Enter City: ");
+                                string city = Console.ReadLine();
+                                person.City = city;
+                                Console.WriteLine("Enter State: ");
+                                string state = Console.ReadLine();
+                                person.State = state;
+                                Console.WriteLine("Enter pin code: ");
+                                int pin = int.Parse(Console.ReadLine());
+                                person.PostalCode = pin;
+                                Console.WriteLine("Enter Phone Number: ");
+                                long number = long.Parse(Console.ReadLine());
+                                person.PhoneNumber = number;
+                                Console.WriteLine("Enter Email id: ");
+                                string email = Console.ReadLine();
+                                person.Email = email;
+                                ad3.addPerson(person);
+                                break;
+
+                            case 2:
+                                ad3.Display();
+                                break;
+                            case 3:
+                                Console.WriteLine("Enter First name to Edit that particular Item: ");
+                                string name = Console.ReadLine();
+                                ad3.EditPerson(name);
+                                break;
+                            case 4:
+                                Console.WriteLine("Enter First name to Delete that particular Item: ");
+                                string name1 = Console.ReadLine();
+                                ad3.Remove(name1);
+                                break;
+                            default:
+                                Console.WriteLine("Please Select the proper operation");
+                                break;
+                        }
+                    }
+                    break;
+
+                    default :
+                    Console.WriteLine("Please select any one operation ");
+                    break;
+            }
         }
     }
 }
--- UC_Program.cs
+++ UC_Program.cs
@@ -28,7 +28,44 @@ namespace AddressBookManagement
         }
     }
 
-    internal class UC_Program
+    public class Person2
+    {
+        public string First_Name;
+        public string Last_Name;
+        public string Address;
+        public string City;
+        public string State;
+        public int PostalCode;
+        public long PhoneNumber;
+        public string Email;
+
+        public string Result2()
+        {
+            return "\nName is: " + First_Name + " " + Last_Name + "\nAddress: " + Address + "\nCity is " + City +
+                "\nState is " + State + "\nPostal code is: " + PostalCode + "\nPhone: " + PhoneNumber + "\nEmail is " + Email;
+        }
+    }
+
+    public class Person3
+    {
+        public string First_Name;
+        public string Last_Name;
+        public string Address;
+        public string City;
+        public string State;
+        public int PostalCode;
+        public long PhoneNumber;
+        public string Email;
+
+        public string Result3()
+        {
+            return "\nName is: " + First_Name + " " + Last_Name + "\nAddress: " + Address + "\nCity is " + City +
+                "\nState is " + State + "\nPostal code is: " + PostalCode + "\nPhone: " + PhoneNumber + "\nEmail is " + Email;
+        }
+    }
+
+    //First AddressBook
+    internal class AddressBook1
     {
         public List<Person> person = new List<Person>();
 
@@ -48,7 +85,7 @@ namespace AddressBookManagement
             File.WriteAllText(@"D:\.Net\Assignment\day10\AddressBookManagement\AddressBookManagement\result.json", jsonData);
         }
 
-        public UC_Program()
+        public AddressBook1()
         {
             string json = File.ReadAllText(@"D:\.Net\Assignment\day10\AddressBookManagement\AddressBookManagement\result.json");
             if (json.Length > 0)
@@ -105,5 +142,159 @@ namespace AddressBookManagement
 
     }
 
+    //Second Address Book
+    internal class AddressBook2
+    {
+        public List<Person2> person2 = new List<Person2>();
+
+        public void Display()
+        {
+            Console.WriteLine("\nAddressBook is: ");
+            foreach (Person2 per in person2)
+            {
+                Console.WriteLine(per.Result2());
+            }
+        }
+
+        public void addPerson(Person2 p2)
+        {
+            person2.Add(p2);
+            string jsonData = JsonConvert.SerializeObject(person2);
+            File.WriteAllText(@"D:\.Net\Assignment\day10\AddressBookManagement\AddressBookManagement\AddressBook2.json", jsonData);
+        }
+
+        public AddressBook2()
+        {
+            string json = File.ReadAllText(@"D:\.Net\Assignment\day10\AddressBookManagement\AddressBookManagement\AddressBook2.json");
+            if (json.Length > 0)
+            {
+                person2 = JsonConvert.DeserializeObject<List<Person2>>(json);
+            }
+            else
+                person2 = new List<Person2>();
+        }
+
+        public void EditPerson(string First_Name)
+        {
+            for (int i = 0; i < person2.Count; i++)
+            {
+                if (person2[i].First_Name == First_Name)
+                {
+                    Console.WriteLine("Enter First Name: ");
+                    person2[i].First_Name = Console.ReadLine();
+                    Console.WriteLine("Enter Last Name: ");
+                    person2[i].Last_Name = Console.ReadLine();
+                    Console.WriteLine("Enter Address ");
+                    person2[i].Address = Console.ReadLine();
+                    Console.WriteLine("Enter City: ");
+                    person2[i].City = Console.ReadLine();
+                    Console.WriteLine("Enter State: ");
+                    person2[i].State = Console.ReadLine();
+                    Console.WriteLine("Enter pin code: ");
+                    person2[i].PostalCode = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter Phone Number: ");
+                    person2[i].PhoneNumber = long.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter Email id: ");
+                    person2[i].Email = Console.ReadLine();
+                }
+            }
+            string jsonData = JsonConvert.SerializeObject(person2);
+            File.WriteAllText(@"D:\.Net\Assignment\day10\AddressBookManagement\AddressBookManagement\AddressBook2.json", jsonData);
+        }
+
+        public void Remove(string First_Name)
+        {
+            Person2 pers = null;
+            foreach (Person2 p2 in person2)
+            {
+                if (p2.First_Name == First_Name)
+                {
+                    pers = p2;
+                }
+            }
+            person2.Remove(pers);
+            string jsonData = JsonConvert.SerializeObject(person2);
+            File.WriteAllText(@"D:\.Net\Assignment\day10\AddressBookManagement\AddressBookManagement\AddressBook2.json", jsonData);
+        }
+
+    }
+
+    //Third AddressBook
+    internal class AddressBook3
+    {
+        public List<Person3> person = new List<Person3>();
+
+        public void Display()
+        {
+            Console.WriteLine("\nAddressBook is: ");
+            foreach (Person3 per in person)
+            {
+                Console.WriteLine(per.Result3());
+            }
+        }
+
+        public void addPerson(Person3 p3)
+        {
+            person.Add(p3);
+            string jsonData = JsonConvert.SerializeObject(person);
+            File.WriteAllText(@"D:\.Net\Assignment\day10\AddressBookManagement\AddressBookManagement\AddressBook2.json", jsonData);
+        }
+
+        public AddressBook3()
+        {
+            string json = File.ReadAllText(@"D:\.Net\Assignment\day10\AddressBookManagement\AddressBookManagement\AddressBook3.json");
+            if (json.Length > 0)
+            {
+                person = JsonConvert.DeserializeObject<List<Person3>>(json);
+            }
+            else
+                person = new List<Person3>();
+        }
+
+        public void EditPerson(string First_Name)
+        {
+            for (int i = 0; i < person.Count; i++)
+            {
+                if (person[i].First_Name == First_Name)
+                {
+                    Console.WriteLine("Enter First Name: ");
+                    person[i].First_Name = Console.ReadLine();
+                    Console.WriteLine("Enter Last Name: ");
+                    person[i].Last_Name = Console.ReadLine();
+                    Console.WriteLine("Enter Address ");
+                    person[i].Address = Console.ReadLine();
+                    Console.WriteLine("Enter City: ");
+                    person[i].City = Console.ReadLine();
+                    Console.WriteLine("Enter State: ");
+                    person[i].State = Console.ReadLine();
+                    Console.WriteLine("Enter pin code: ");
+                    person[i].PostalCode = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter Phone Number: ");
+                    person[i].PhoneNumber = long.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter Email id: ");
+                    person[i].Email = Console.ReadLine();
+                }
+            }
+            string jsonData = JsonConvert.SerializeObject(person);
+            File.WriteAllText(@"D:\.Net\Assignment\day10\AddressBookManagement\AddressBookManagement\AddressBook3.json", jsonData);
+        }
+
+        public void Remove(string First_Name)
+        {
+            Person3 pers = null;
+            foreach (Person3 p3 in person)
+            {
+                if (p3.First_Name == First_Name)
+                {
+                    pers = p3;
+                }
+            }
+            person.Remove(pers);
+            string jsonData = JsonConvert.SerializeObject(person);
+            File.WriteAllText(@"D:\.Net\Assignment\day10\AddressBookManagement\AddressBookManagement\AddressBook3.json", jsonData);
+        }
+
+
+    }
 
 }
