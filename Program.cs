3e6b1cc5048fd302a7ab0795f26cec67cc769509
Abhilash <abhilashmeher1234@gmail.com>
2022-03-20 12:47:17 +0530

    Added UC_1 program files

--- Program.cs
+++ Program.cs
@@ -7,6 +7,19 @@ namespace AddressBookManagement
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To AddressBook Program");
+
+            UC_Program uC_1 = new UC_Program();
+            Person person = new Person();
+            person.First_Name = "Abhilash";
+            person.Last_Name = "Meher";
+            person.Address = "Odisha";
+            person.City = "Balangir";
+            person.State = "Odisha";
+            person.PostalCode = 767025;
+            person.PhoneNumber = 4578632;
+            person.Email = "abhilashmeher1234@gmail.com";
+            uC_1.addPerson(person);
+            uC_1.Display();
         }
     }
 }
---
+++ UC_Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+
+namespace AddressBookManagement
+{
+
+    public class Person
+    {
+        public string First_Name;
+        public string Last_Name;
+        public string Address;
+        public string City;
+        public string State;
+        public int PostalCode;
+        public long PhoneNumber;
+        public string Email;
+
+        public string Result()
+        {
+            return "Name is: " + First_Name + Last_Name + "\nAddress: " + Address + "\nCity is " + City +
+                "\nState is " + State + "\nPostal code is: " + PostalCode + "\nPhone: " + PhoneNumber + "\nEmail is " + Email;
+        }
+    }
+
+    internal class UC_Program
+    {
+        public List<Person> person = new List<Person>();
+
+        public void Display()
+        {
+            Console.WriteLine("Welcome to Program");
+            foreach (Person per in person)
+            {
+                Console.WriteLine(per.Result());
+            }
+        }
+
+        public void addPerson(Person p)
+        {
+            person.Add(p);
+            string jsonData = JsonConvert.SerializeObject(person);
+            File.WriteAllText(@"D:\.Net\Assignment\day10\AddressBookManagement\AddressBookManagement\result.json", jsonData);
+        }
+
+        public UC_Program()
+        {
+            string json = File.ReadAllText(@"D:\.Net\Assignment\day10\AddressBookManagement\AddressBookManagement\result.json");
+            if (json.Length > 0)
+            {
+                person = JsonConvert.DeserializeObject<List<Person>>(json);
+            }
+            else
+                person = new List<Person>();
+        }
+
+    }
+
+
+}
